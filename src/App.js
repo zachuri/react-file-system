9741f6c88e85529c27d0796153daa51a53c7295c
ZACHURI <punsalang.zachary@gmail.com>
2021-12-29 14:55:49 -0800

    added function to be able to view and unview children components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
 	return (
 		<div>
 			<Folder name="Desktop">
-				<Files name="file.txt" />
-				<Files name="pic.jpeg" />
+				<Folder name="Music">
+					<Files name="file.txt" />
+					<Files name="pic.jpeg" />
+				</Folder>
 			</Folder>
 			<Folder name="Applications">
 				<Files name="hw.txt" />
@@ -17,14 +19,26 @@ export default function App() {
 }
 
 const Folder = (props) => {
-	console.log(props);
+	// console.log(props);
 
+	// Uncomment if you don't what to write props
+	// const {name, children, isOpen] = props;
+
+	const [isOpen, setIsOpen] = useState(true);
+
+	// Border to Show seperations
 	const parentStyle = { border: '2px solid pink' };
 	const childStyle = { marginLeft: '17px' };
+
+	// Function to handle isOpen
+	const handleIsOpen = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
 		<div style={parentStyle}>
-			{props.name}
-			<div style={childStyle}>{props.children}</div>
+			<span onClick={handleIsOpen}>{props.name}</span>
+			<div style={childStyle}>{isOpen ? props.children : null}</div>
 		</div>
 	);
 };
